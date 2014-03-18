e625fbc126672b7249dbaf63b8d68261f673377c
Matt Vollrath <matt@endpoint.com>
2014-03-17 21:26:23 -0400

    Cleanup import in evdev.reader loop

--- src/main/java/com/endpoint/lg/evdev/reader/EvdevReaderLoop.java
+++ src/main/java/com/endpoint/lg/evdev/reader/EvdevReaderLoop.java
@@ -23,7 +23,6 @@ import interactivespaces.util.concurrency.CancellableLoop;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.FileNotFoundException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.List;
